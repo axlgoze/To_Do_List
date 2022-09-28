06bc65ebbb1a8351a3bc85be8bd49b7c99e97728
Axel Reyes <rbaxel.f@gmail.com>
2022-09-28 18:06:56 -0500

    Clase03 Completada

--- script.js
+++ script.js
@@ -3,7 +3,39 @@ const btn = document.querySelector("[data-form-btn]");
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
+    const list = document.querySelector("[data-list]");
+
+    const value = input.value;
+    
+    const task = document.createElement("li");
+    const taskContent = document.createElement("div");
+    const titleTask = document.createElement("span");
+    
+    titleTask.classList.add("task");
+    task.classList.add("card");
+    titleTask.innerText = value;
+
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(titleTask);
+    task.appendChild(taskContent);
+    list.appendChild(task);
+
+    const content = `
+    ${checkComplete()}
+    <span class="task">${value}</span>
+    
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+
+    // task.innerHTML = content;
+    input.value= '';
 }
 
 btn.addEventListener("click", createTask);
+
+const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add("far");
+    i.classList.add("fa-check-square");
+    i.classList.add("icon");
+    return i;
+}
