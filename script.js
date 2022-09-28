4b5698a7aa31521c7c00ceefdfac67fc6d2545be
Axel Reyes <rbaxel.f@gmail.com>
2022-09-28 18:34:54 -0500

    Clase04 Completada

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const btn = document.querySelector("[data-form-btn]");
+( () => {
+
+    const btn = document.querySelector("[data-form-btn]");
 
 const createTask = (evento) => {
     evento.preventDefault();
@@ -34,8 +36,16 @@ btn.addEventListener("click", createTask);
 
 const checkComplete = () => {
     const i = document.createElement("i");
-    i.classList.add("far");
-    i.classList.add("fa-check-square");
-    i.classList.add("icon");
+    i.classList.add("far","fa-check-square", "icon");
+    i.addEventListener("click",completeTask);
     return i;
 }
+
+const completeTask = (event) =>{
+    const element = event.target;
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
+}
+
+}) ();
