2c425e32388ad9d399745221644aa04d64e8f18e
Axel Reyes <rbaxel.f@gmail.com>
2022-09-29 18:01:59 -0500

    Curso02-Clase01 Terminada

--- script.js
+++ script.js
@@ -3,13 +3,25 @@ import deleteIcon from "./components/deleteIcon.js";
 // ( () => {
 
     const btn = document.querySelector("[data-form-btn]");
-
+// Agregar la tarea
+const addTasK = (evento) => {
+    const list = document.querySelector("[data-list]");
+    // llamada a la funcion
+    const task = createTask(evento);
+    list.appendChild(task);
+}
+// crear estructura HTML
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
-    const list = document.querySelector("[data-list]");
-
+    const calendar = document.querySelector("[data-form-date]");
+    const date=calendar.value;
+    const dateFormat = moment(date).format("DD/MM/YYYY");
+    
     const value = input.value;
+    
+    const dateElement= document.createElement("span");
+    dateElement.innerHTML = dateFormat;
     
     const taskContent = document.createElement("div");
     const task = document.createElement("li");
@@ -24,11 +36,13 @@ const createTask = (evento) => {
     taskContent.appendChild(titleTask);
 
     task.appendChild(taskContent);
+    task.appendChild(dateElement);
     task.appendChild(deleteIcon());
-    list.appendChild(task);
+    
 
     input.value= '';
+    return task;
 }
 
-btn.addEventListener("click", createTask);
+btn.addEventListener("click", addTasK);
 // }) ();
